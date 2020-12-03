a5e8642b34cef75bbfe9a58f6c5e3ceb6578be14
JMcB#7918 <joel.mcbride1@live.com>
2020-12-03 17:26:50 +0000

    Finish part one solution?

--- Day-3-Toboggan-Trajectory/toboggan-trajectory.py
+++ Day-3-Toboggan-Trajectory/toboggan-trajectory.py
@@ -11,4 +11,15 @@ for line in input_raw.splitlines():
     row_list = [char_map[char] for char in line]
     input_2d_list.append(row_list)
 
-pass
+x_coord = 0
+tree_collisions = 0
+for input_line in input_2d_list:
+    if x_coord > len(input_line):
+        x_coord -= len(input_line)
+
+    if input_line[x_coord]:
+        tree_collisions += 1
+
+    x_coord += 3
+
+print(tree_collisions)
