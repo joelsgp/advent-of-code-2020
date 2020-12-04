44ebe542688dba8e56713d6eeb19d16494f6dfae
JMcB17 <joel.mcbride1@live.com>
2020-12-04 13:06:38 +0000

    Solve part one

---
+++ Day-4-Passport-Processing/day-4-part-two.py
@@ -0,0 +1,30 @@
+def check_passport(passport, required_fields):
+    for field in required_fields:
+        if field not in passport:
+            return False
+
+    return True
+
+with open('input.txt', 'r') as input_file:
+    input_raw = input_file.read()
+
+passports = input_raw.split('\n\n')
+
+required_fields = [
+        'byr',
+        'iyr',
+        'eyr',
+        'hgt',
+        'hcl',
+        'ecl',
+        'pid',
+##        'cid',
+    ]
+
+valid_passports = 0
+for passport in passports:
+    if check_passport(passport, required_fields):
+        valid_passports += 1
+
+print(valid_passports)
+    
