89ac37e196f04804e539710cec238a7138ae7c6e
JMcB#7918 <joel.mcbride1@live.com>
2020-12-06 14:53:32 +0000

    Part two done, a little hacky but whatever

--- Day-5-Binary-Boarding/binary-boarding.py
+++ Day-5-Binary-Boarding/binary-boarding.py
@@ -26,13 +26,22 @@ def seat_id_from_partition(partition):
     column = binary_space_partition(partition[7:], 0, 7, 'R', 'L')
 
     seat_id = (row * 8) + column
-    return seat_id
+    return row, column, seat_id
 
 
+available_seats = [[j for j in range(0, 7+1)] for i in range(0, 127+1)]
 highest_id = 0
 for seat_partition in partitions:
-    partition_seat_id = seat_id_from_partition(seat_partition)
+    partition_row, partition_column, partition_seat_id = seat_id_from_partition(seat_partition)
     if partition_seat_id > highest_id:
         highest_id = partition_seat_id
 
-print(highest_id)
+    available_seats[partition_row] = [column for column in available_seats[partition_row] if column != partition_column]
+
+for row_number, row_list in enumerate(available_seats):
+    if len(row_list) not in [0, 7+1]:
+        your_seat_id = (row_number * 8) + row_list[0]
+        print(f'your seat ID: {your_seat_id}')
+        break
+
+print(f'highest seat id: {highest_id}')
