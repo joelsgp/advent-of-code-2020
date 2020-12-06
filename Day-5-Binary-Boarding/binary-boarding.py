04171292d4b8b44e4f44542b0b9680578422083b
JMcB#7918 <joel.mcbride1@live.com>
2020-12-06 13:23:00 +0000

    Create main part of program
    Just need to implement binary search algorithm now
    
    Going to eat lunch, time until next commit doesn't count in my completion time for this challenge

--- Day-5-Binary-Boarding/binary-boarding.py
+++ Day-5-Binary-Boarding/binary-boarding.py
@@ -1,3 +1,18 @@
 with open('input', 'r') as input_file:
     input_raw = input_file.read()
 
+partitions = input_raw.splitlines()
+
+
+def seat_id_from_partition(partition):
+    # TODO: implement
+    return 0
+
+
+highest_id = 0
+for partition in partitions:
+    partition_seat_id = seat_id_from_partition(partition)
+    if partition_seat_id > highest_id:
+        highest_id = partition_seat_id
+
+print(highest_id)
